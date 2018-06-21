da434303cca8af411460e9b6169cf5fe2badfd1a
Sarang Sohail <sarangsohail@gmail.com>
2018-06-21 15:28:25 +0100

    ordering system

--- app/src/main/java/com/example/carwash/BookingActivity.java
+++ app/src/main/java/com/example/carwash/BookingActivity.java
@@ -1,19 +1,34 @@
 package com.example.carwash;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 
 public class BookingActivity extends AppCompatActivity {
+
+    private Button orderButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
         hideKeyboard(this);
+
+        orderButton = (Button) findViewById(R.id.orderButton);
+        orderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loadCustomerInformationActivity = new Intent(getApplicationContext(), OrderingActivity.class);
+                startActivity(loadCustomerInformationActivity);
+            }
+        });
+
     }
+
 
     //when the BookingActivity is loaded, hide the keyboard
     public static void hideKeyboard(Activity activity) {
