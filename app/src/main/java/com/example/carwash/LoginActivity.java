4dab8dac6a484bbc2d3950db02e8a6b922735214
Sarang Sohail <sarangsohail@gmail.com>
2018-06-21 13:23:02 +0100

    booking activity

--- app/src/main/java/com/example/carwash/LoginActivity.java
+++ app/src/main/java/com/example/carwash/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.carwash;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -44,7 +45,14 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      */
     private static final int REQUEST_READ_CONTACTS = 0;
 
-     /**
+    /**
+     * A dummy authentication store containing known user names and passwords.
+     * TODO: remove after connecting to a real authentication system.
+     */
+    private static final String[] DUMMY_CREDENTIALS = new String[]{
+            "foo@example.com:hello", "bar@example.com:world"
+    };
+    /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
     private UserLoginTask mAuthTask = null;
@@ -190,8 +198,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
     private boolean isPasswordValid(String password) {
         //TODO: Replace this with your own logic
-
-        return password.length() > 6;
+        return password.length() > 4;
     }
 
     /**
@@ -327,7 +334,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-                finish();
+
+            Intent loadBookingActivity = new Intent(getApplicationContext(), BookingActivity.class);
+            startActivity(loadBookingActivity);
+            finish();
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
