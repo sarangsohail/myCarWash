2b979e8abde6c294fdcf8b12564630c5fdd45b88
Sarang Sohail <sarangsohail@gmail.com>
2018-06-22 18:51:47 +0100

    fixed tab swithching individuality

--- app/src/main/java/com/example/carwash/OrderingActivity.java
+++ app/src/main/java/com/example/carwash/OrderingActivity.java
@@ -11,6 +11,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -91,42 +92,6 @@ public class OrderingActivity extends AppCompatActivity {
     }
 
     /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_ordering, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-
-  //          textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-
-    /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
      */
@@ -138,9 +103,22 @@ public class OrderingActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+
+            switch (position){
+
+                case 0:
+                    contactDetailsActivity contactDetailsActivity = new contactDetailsActivity();
+                    return contactDetailsActivity;
+                case 1:
+                    serviceWashActivity serviceWashActivity = new serviceWashActivity();
+                    return serviceWashActivity;
+                case 2:
+                    paymentActivity paymentActivity = new paymentActivity();
+                    default:
+                        return null;
+            }
+
+
         }
 
         @Override
@@ -149,4 +127,5 @@ public class OrderingActivity extends AppCompatActivity {
             return 3;
         }
     }
+
 }
