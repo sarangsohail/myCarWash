669d5ff81d8cc26daeecbe0e94a0e1cd0191e197
Sarang Sohail <sarangsohail@gmail.com>
2018-07-09 23:03:12 +0100

    current location tracking

--- app/src/main/java/com/example/carwash/contactDetails.java
+++ app/src/main/java/com/example/carwash/contactDetails.java
@@ -1,12 +1,18 @@
 package com.example.carwash;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.ContentUris;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.AppCompatTextView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,42 +20,88 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
 public class contactDetails extends Fragment {
 
-    private static final String TAG = "Contact Details Fragment";
+    private static final String TAG = "Contact-Details-Fragmnt";
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
     private static final float DEFAULT_ZOOM = 15f;
 
-
+    private FusedLocationProviderClient mFusedLocationProviderClient;
     private Boolean mLocationPermissionsGranted = false;
 
+    double currentLAT = 0;
+    double currentLOC = 0;
     @Override
-    public View onCreateView(LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
-        View view =  inflater.inflate(R.layout.contactdetails, container, false);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getLocationPermission();
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.contactdetails, container, false);
         return view;
     }
-    private void getLocationPermission(){
-        Log.d(TAG, "getLocationPermission: getting location permissions");
+
+    private void getDeviceLocation(){
+
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
+
+        try{
+            if (mLocationPermissionsGranted){
+
+                Task location = mFusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()){
+                            Log.d(TAG, "onComplete: found location!");
+                            Location currentLocation = (Location) task.getResult();
+                            currentLAT = currentLocation.getLatitude();
+                            currentLOC = currentLocation.getLongitude();
+                        }else{
+                            Log.d(TAG, "onComplete: current location is not found/null");
+                            Toast.makeText(getContext(), "unable to get current location", Toast.LENGTH_SHORT).show();
+
+
+                        }
+                    }
+                })
+
+            }
+        }catch (SecurityException e){
+
+            Log.e(TAG, "get device location : Security Exception "  +e.getMessage());
+        }
+
+
+    }
+    private void getLocationPermission() {
         String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
                 Manifest.permission.ACCESS_COARSE_LOCATION};
 
-        if(ContextCompat.checkSelfPermission(this.getContext(),
-                FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-            if(ContextCompat.checkSelfPermission(this.getContext(),
-                    COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+        if (ContextCompat.checkSelfPermission(this.getContext(),
+                FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this.getContext(),
+                    COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                 mLocationPermissionsGranted = true;
-            }else{
-                ActivityCompat.requestPermissions(this,
+            } else {
+                ActivityCompat.requestPermissions(getActivity(),
                         permissions,
                         LOCATION_PERMISSION_REQUEST_CODE);
             }
-        }else{
-            ActivityCompat.requestPermissions(this,
+        } else {
+            ActivityCompat.requestPermissions(getActivity(),
                     permissions,
                     LOCATION_PERMISSION_REQUEST_CODE);
         }
     }
-
 }
