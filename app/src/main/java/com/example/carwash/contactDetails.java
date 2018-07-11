9c5cb0ac9db790db17cb18135999834b56f18b94
Sarang Sohail <sarangsohail@gmail.com>
2018-07-11 10:25:01 +0100

    reverse geocoding nearly done, yet to deal with context problem

--- app/src/main/java/com/example/carwash/contactDetails.java
+++ app/src/main/java/com/example/carwash/contactDetails.java
@@ -12,13 +12,26 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.TaskStackBuilder;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.widget.AppCompatTextView;
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.AsyncTask;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Switch;
 import android.widget.Toast;
@@ -29,7 +42,9 @@ import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
-public class contactDetails extends Fragment {
+import static android.content.ContentValues.TAG;
+
+public class contactDetails extends Fragment  {
 
     private static final String TAG = "Contact-Details-Fragmnt";
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
@@ -56,11 +71,13 @@ public class contactDetails extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.contactdetails, container, false);
         final Switch sw = (Switch) view.findViewById(R.id.locationSwitch);
+        final EditText firstLineAddress = (EditText) view.findViewById(R.id.first_line_address_et1);
 
         sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
                     getDeviceLocation();
+                    firstLineAddress.setText(getString(R.string.address_text));
                 } else {
 
                 }
@@ -83,7 +100,8 @@ public class contactDetails extends Fragment {
                         if (task.isSuccessful()) {
                             Log.d(TAG, "onComplete: found location!");
                             Location currentLocation = (Location) task.getResult();
-
+                            //todo: sort this prpblem out
+                            new FetchAddressTask(getActivity(), getContext()).execute(currentLocation);
 
                         } else {
                             Log.d(TAG, "onComplete: current location is not found/null");
@@ -119,5 +137,67 @@ public class contactDetails extends Fragment {
                     permissions,
                     LOCATION_PERMISSION_REQUEST_CODE);
         }
+    }
+}
+public class FetchAddressTask extends AsyncTask {
+
+    private Context mContext;
+    private OnTaskCompleted mListener;
+
+    FetchAddressTask(Context applicationContext, OnTaskCompleted onTaskCompleted){
+        mContext = applicationContext;
+    }
+
+    interface OnTaskCompleted{
+        void onTaskCompleted(String result);
+
+    }
+    @Override
+    protected Object doInBackground(Object[] params) {
+
+        Geocoder geocoder = new Geocoder(mContext, Locale.getDefault());
+        Location location = params[0];
+        List<Address> addressList = null;
+        String resultMessage = "";
+
+        try {
+            addressList = geocoder.getFromLocation(
+                    location.getLatitude(),
+                    location.getLongitude(),
+                    1);
+            if (addressList == null || addressList.size() == 0) {
+                if (resultMessage.isEmpty()) {
+                    resultMessage = mContext
+                            .getString(R.string.no_address_found);
+                    Log.e(TAG, resultMessage);
+                }else{
+                    //reverse geocding was successful
+                    Address address = addressList.get(0);
+                    ArrayList<String> addressParts = new ArrayList<>();
+
+                    for (int i = 0; i <= address.getMaxAddressLineIndex(); i++) {
+                        addressParts.add(address.getAddressLine(i));
+                    }
+
+                    resultMessage = TextUtils.join("\n", addressParts);
+                }
+            }
+        } catch (IOException e) {
+            // Catch invalid latitude or longitude values
+            resultMessage = mContext
+                    .getString(R.string.invalid_lat_long_used);
+            Log.e(TAG, resultMessage + ". " +
+                    "Latitude = " + location.getLatitude() +
+                    ", Longitude = " +
+                    location.getLongitude(), e);
+        }
+
+        return resultMessage;
+    }
+
+    @Override
+    protected void onPostExecute(Object o) {
+        mListener.onTaskCompleted(o.toString());
+        super.onPostExecute(o.toString());
     }
 }
