eddc1660cdf118f4de096b5ac2bead92bdc5524e
Sarang Sohail <sarangsohail@gmail.com>
2018-07-18 19:22:39 +0100

    service wash fragment tweaks

--- app/src/main/java/com/example/carwash/serviceWash.java
+++ app/src/main/java/com/example/carwash/serviceWash.java
@@ -6,14 +6,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class serviceWash extends Fragment {
 
 
-    @Nullable
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        }
+
     @Override
     public View onCreateView(LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.service_wash, container, false);
@@ -25,6 +35,52 @@ public class serviceWash extends Fragment {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
+
+        CheckBox interiorCk =   view.findViewById(R.id.interiorWash_check);
+        interiorCk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (((CheckBox) view).isChecked()) {
+                    TextView interiorPriceTV = (TextView) getView().findViewById(R.id.interiorWashPrice_tv);
+                    interiorPriceTV.setVisibility(View.VISIBLE);
+                }
+                else{
+                    TextView interiorPriceTV = (TextView) getView().findViewById(R.id.interiorWashPrice_tv);
+                    interiorPriceTV.setVisibility(View.INVISIBLE);
+
+                }
+            }
+        });
+        CheckBox exteriorCk = view.findViewById(R.id.exteriorWash_check);
+        exteriorCk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (((CheckBox) view).isChecked()) {
+                    TextView exteriorViewTv = (TextView) getView().findViewById(R.id.exteriorWashPrice_tv);
+                    exteriorViewTv.setVisibility(View.VISIBLE);
+                }
+                else{
+                    TextView exteriorViewTv = (TextView) getView().findViewById(R.id.exteriorWashPrice_tv);
+                    exteriorViewTv.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+        CheckBox interiorExteriorCk = view.findViewById(R.id.interior_exterior_wash);
+        interiorExteriorCk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (((CheckBox) view).isChecked()) {
+                    TextView interExterckTV = (TextView) getView().findViewById(R.id.interior_exterior_wash);
+                    interExterckTV.setVisibility(View.VISIBLE);
+                }
+                else{
+                    TextView interExterckTV = (TextView) getView().findViewById(R.id.interior_exterior_wash);
+                    interExterckTV.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
         return view;
     }
+
 }
