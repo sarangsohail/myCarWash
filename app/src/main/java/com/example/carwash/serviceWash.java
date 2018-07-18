2ee5f2929ce91da7ac8927efd0d99424e6a15bc0
Sarang Sohail <sarangsohail@gmail.com>
2018-07-18 18:18:26 +0100

    service wash fragment tabs

--- app/src/main/java/com/example/carwash/serviceWash.java
+++ app/src/main/java/com/example/carwash/serviceWash.java
@@ -6,10 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class serviceWash extends Fragment {
-
 
 
     @Nullable
@@ -17,8 +18,12 @@ public class serviceWash extends Fragment {
     public View onCreateView(LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.service_wash, container, false);
 
-
-
+        //for the car size spinner/drop down menu
+        Spinner spinner = (Spinner) view.findViewById(R.id.car_size_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.CarSize, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
         return view;
     }
