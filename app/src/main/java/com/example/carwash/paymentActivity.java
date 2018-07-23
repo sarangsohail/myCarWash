36ecd1f7c4e6331925c198a4cdbe25f0063f5fec
Sarang Sohail <sarangsohail@gmail.com>
2018-07-23 20:10:27 +0100

    service wash dealt with

--- app/src/main/java/com/example/carwash/paymentActivity.java
+++ app/src/main/java/com/example/carwash/paymentActivity.java
@@ -1,5 +1,8 @@
 package com.example.carwash;
 
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,23 +10,41 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
 
 public class paymentActivity extends Fragment {
 
     private static final String TAG = "Contact Details Fragment";
 
 
-    private Button BTNTest2;
+    private ProgressBar progressBar;
+    private ImageButton paymentIB;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.payment_tab, container, false);
+        //progress bar
+        progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.INVISIBLE);
 
-        BTNTest2 = (Button) view.findViewById(R.id.btnTEST2);
+        //paypal image button
+        paymentIB = (ImageButton) view.findViewById(R.id.paymentIB);
+        paymentIB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
+                String url = "https://www.paypal.com/uk/home";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+                progressBar.setVisibility(View.INVISIBLE);
+            }
 
-
+        });
 
         return view;
     }
 }
-// see if i can intergrate paypal into this fragment or make a bank input UI
+
