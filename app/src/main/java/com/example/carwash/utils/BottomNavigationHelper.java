6beed33ebac33c7c7466753904411169f0fa07ef
Sarang Sohail <sarangsohail@gmail.com>
2018-07-27 15:36:52 +0100

    Animation removal

--- app/src/main/java/com/example/carwash/utils/BottomNavigationHelper.java
+++ app/src/main/java/com/example/carwash/utils/BottomNavigationHelper.java
@@ -1,15 +1,16 @@
 package com.example.carwash.utils;
 
+import android.util.Log;
+
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 //object class to make it easier to setup the bottom navbar for all the separate profile views
-public class BottomNavigationHelper {
+public class BottomNavigationHelper{
 
-    public static void setUpBottomNavigationView(BottomNavigationViewEx bottomNavigationView){
-        bottomNavigationView.enableAnimation(false);
-        bottomNavigationView.enableShiftingMode(false);
-        bottomNavigationView.enableItemShiftingMode(false);
-        bottomNavigationView.setTextVisibility(false);
+    public static void setupBottomNavigationView(BottomNavigationViewEx bottomNavigationViewEx){
+        bottomNavigationViewEx.enableAnimation(false);
+        bottomNavigationViewEx.enableItemShiftingMode(false);
+        bottomNavigationViewEx.enableShiftingMode(false);
+        bottomNavigationViewEx.setTextVisibility(false);
     }
-
 }
--- app/src/main/java/com/example/carwash/washerAccount.java
+++ app/src/main/java/com/example/carwash/washerAccount.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.carwash.utils.BottomNavigationHelper;
+
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class washerAccount extends AppCompatActivity {
@@ -13,11 +14,12 @@ public class washerAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_washer_account);
-    }
 
+        setupBottomNavigation();
+    }
 
     public void setupBottomNavigation(){
         BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
-        BottomNavigationHelper.setUpBottomNavigationView(bottomNavigationViewEx);
+        BottomNavigationHelper.setupBottomNavigationView(bottomNavigationViewEx);
     }
 }
